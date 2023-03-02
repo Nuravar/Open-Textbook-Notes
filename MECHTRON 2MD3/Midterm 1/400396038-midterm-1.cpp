ae7d2d9912f09a99621823295772e7641c9205d8
Varun Kothandaraman <91991164+Nuravar@users.noreply.github.com>
2023-03-02 16:51:54 -0500

    added more for q1

--- MECHTRON 2MD3/Midterm 1/400396038-midterm-1.cpp
+++ MECHTRON 2MD3/Midterm 1/400396038-midterm-1.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <stack>
 #include <string>
+#include <vector>
+#include <ctype.h>
+#include <sstream>
 using namespace std;
+
+
+
+
 
 class Stack {
     private:
@@ -20,6 +27,7 @@ class Stack {
         double pop(){
             double top = ADT.top();
             ADT.pop();
+            //cout << top << endl;
             return top;
         }
         double top() {
@@ -43,8 +51,25 @@ class RPNEvaluator {
     //constructor
     RPNEvaluator(string rpn_exp);
     virtual ~RPNEvaluator() {};
+    
+    //Evaulate function
+    double Evaluate();
+    void PrintStack(Stack s);
+    vector<string> split(string str);
+    bool isNum(string exp);
+};
 
-    /* 
+bool RPNEvaluator::isNum(string exp){
+    for(int i =0;i<exp.length()-1;i++){
+        
+    }
+}
+
+RPNEvaluator::RPNEvaluator(string rpn_exp){
+    expression = rpn_exp;
+}
+
+/* 
     Format for solving question 1
     Step 1: create a stack for all numbers, heere it is rpn_stack which holds doubles
     Step 2: if the item extracted from the string is a number push to the stack but if it is a operator pop last two numbers, evaluate 
@@ -52,53 +77,66 @@ class RPNEvaluator {
     Step 3: continue until you are at the end of the string
     Step 4: if you are at the end of the string you should have only one number left (ie: size = 1) and pop/return the last number
     Step 5: if size is not 1 at the end or if the operators are not correct in the switch statement return -1 and print "Error: malformed expression"
-    */
-    //Evaulate function
-    double Evaluate();
-    void PrintStack(Stack s);
-};
-
-
-RPNEvaluator::RPNEvaluator(string rpn_exp){
-    expression = rpn_exp;
-}
-
+*/
 double RPNEvaluator::Evaluate(){
     //remove spaces
-    for(int i =0;i<expression.length()-1;i++){
-        if(expression[i]==' '){
-            expression.erase(i,1);
+    // for(int i =0;i<expression.length()-1;i++){
+    //     if(expression[i]==' '){
+    //         expression.erase(i,1);
+    //     }
+    // }
+    vector<string> exp = split(expression);
+    //check if stack works
+    for(int i =0;i<exp.size();i++){
+        //double num = atof(expression[i]);
+        if (exp[i] != "+", "-", "*", "/" && isdigit(exp[i])){
+            rpn_stack.push((double)expression[i] - '0');
+        } else {
+            double num2 = rpn_stack.pop();
+            double num1 = rpn_stack.pop();
+            switch (expression[i]){
+            case '+':
+                rpn_stack.push(num1+num2);
+                break;
+            case '-':
+                rpn_stack.push(num1-num2);
+                break;
+            case '*':
+                rpn_stack.push(num1*num2);
+                break;
+            case '/':
+                rpn_stack.push(num1/num2);
+                break;
+            default:
+                printf("Error: malformed expression");
+                return -1;
+                break;
+            }
         }
     }
-    //check if stack works
-    for(int i =0;i<expression.length();i++){
-        rpn_stack.push(expression[i]);
-    }
     PrintStack(rpn_stack);
-
+    return rpn_stack.top();
 }
 
 void RPNEvaluator::PrintStack(Stack s){
-    // If stack is empty then return
     if (s.isEmpty())
         return;
-     
- 
-    int x = s.top();
- 
-    // Pop the top element of the stack
+
+    double x = s.top();
     s.pop();
- 
-    // Recursively call the function PrintStack
     PrintStack(s);
- 
-    // Print the stack element starting
-    // from the bottom
     cout << x << " ";
- 
-    // Push the same element onto the stack
-    // to preserve the order
     s.push(x);
+}
+
+vector<string> RPNEvaluator::split(string str){
+    vector<string> equation;
+    stringstream ss(str);
+    string token;
+    while(getline(ss, token, ' ')) { 
+    equation.push_back(token); 
+    } 
+    return equation; 
 }
 
 int main(){
