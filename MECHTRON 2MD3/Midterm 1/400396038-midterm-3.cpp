a2410c8a6efd52fb95364bbbade88c39c432d5f8
Varun Kothandaraman <91991164+Nuravar@users.noreply.github.com>
2023-03-03 18:14:39 -0500

    updated q3

--- MECHTRON 2MD3/Midterm 1/400396038-midterm-3.cpp
+++ MECHTRON 2MD3/Midterm 1/400396038-midterm-3.cpp
@@ -2,61 +2,88 @@
 #include <string>
 #include <vector>
 #include <ctype.h>
-#include <deque>
+#include <algorithm>    // std::rotate
 
 
 
 class Deque {
     private:
-        int size; 
-        std::vector<std::string> ADT;
+        int n; //size
+        int f; //front
+        int r; //rear
+        int currentN;
+        std::vector<std::string> ADT; //circular vector
     public:
-        Deque(int n){ //(a)
-            size = n;
-            ADT.reserve(size);
+        Deque(int size){
+            n = size;
+            f = 0;
+            r = 0;
+            currentN = 0;
+            ADT.reserve(size); 
         }
-        ~Deque(){}
         void insertFront(std::string e);
         void insertBack(std::string e);
         void eraseFront();
         void eraseBack();
-        std::string front();
-        std::string back();
-        int size();
-        bool empty();
+        void front();
+        void back();
+        void size();
+        void empty();
 };
 
-void Deque::insertFront(std::string e){
+void insertFront(std::string e){
 
 }
-void Deque::insertBack(std::string e){
+void insertBack(std::string e){
 
 }
-void Deque::eraseFront(){
+void eraseFront(){
 
 }
-void Deque::eraseBack(){
-    if(!empty()){
+void eraseBack(){
 
-    }
 }
-std::string Deque::front(){
-    if(!empty()){
-        return ADT.front();
-    } else {
-        return "error: empty deque";
-    }
+void front(){
+
 }
-std::string Deque::back(){
-    if(!empty()){
-        return ADT.back();
-    } else {
-        return "error: empty deque";
-    }
+void back(){
+
 }
-int Deque::size(){
-    return ADT.size();
+void size(){
+    return currentN;
 }
-bool Deque::empty(){
-    return ADT.empty();
+void empty(){
+    return currentN == 0;
+}
+
+
+
+int main(){
+    Deque deque(5); // testing (A)
+    deque.insertBack("2");
+    deque.insertBack("3");
+    deque.insertBack("4");
+    deque.insertBack("5"); 
+    deque.insertFront("1");
+    // deque.printVector(); // testing insert front (B) and back (C)
+    deque.insertBack("6");
+    // deque.printVector(); // testing circular insert back (C)
+    deque.insertFront("7");
+    // deque.printVector(); // testing circular insert front (D)
+    deque.eraseBack();
+    deque.eraseBack();
+    deque.eraseBack();
+    deque.eraseBack();
+    deque.eraseFront();
+    deque.eraseFront();
+    std::string test1 = deque.front();
+    std::string test2 = deque.back();
+    // std::cout << test1 << "|" << test2 <<std::endl; //testing error for front and back
+    // deque.printVector(); //testing erase back (E) and front (F)
+    deque.insertBack("2");
+    deque.insertBack("3");
+    deque.insertBack("4");
+    deque.insertBack("5"); 
+    deque.insertFront("1");
+
 }
