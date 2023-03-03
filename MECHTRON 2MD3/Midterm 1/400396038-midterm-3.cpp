06d8b6121395490df90f9c5253c40b943b5e31c3
Varun Kothandaraman <91991164+Nuravar@users.noreply.github.com>
2023-03-03 18:29:20 -0500

    updated q3

--- MECHTRON 2MD3/Midterm 1/400396038-midterm-3.cpp
+++ MECHTRON 2MD3/Midterm 1/400396038-midterm-3.cpp
@@ -11,49 +11,74 @@ class Deque {
         int n; //size
         int f; //front
         int r; //rear
-        int currentN;
+        int N;
         std::vector<std::string> ADT; //circular vector
     public:
         Deque(int size){
-            n = size;
+            N = size;
             f = 0;
             r = 0;
-            currentN = 0;
+            n = 0;
             ADT.reserve(size); 
         }
         void insertFront(std::string e);
         void insertBack(std::string e);
         void eraseFront();
         void eraseBack();
-        void front();
-        void back();
-        void size();
-        void empty();
+        std::string front();
+        std::string back();
+        int size();
+        bool empty();
 };
 
-void insertFront(std::string e){
+void Deque::insertFront(std::string e){
+    if (n == N){
+        return;
+    } 
+    f = (f - 1 + N) % N;
+    ADT[f] = e;
+    n++;
 
 }
-void insertBack(std::string e){
-
+void Deque::insertBack(std::string e){
+    if (n == N){
+        return;
+    } 
+    ADT[r] = e;
+    r = (r + 1) % N;
+    n++;
 }
-void eraseFront(){
-
+void Deque::eraseFront(){
+    if (n==0){
+        return;
+    }
+    f = (f - 1 + N) % N;
+    n--;
 }
-void eraseBack(){
-
+void Deque::eraseBack(){
+    if (n==0){
+        return;
+    }
+    r = (r -1 + N) % N;
+    n--;
 }
-void front(){
-
+std::string Deque::front(){
+    if (n==0){
+        return "error: empty deque";
+    }
+    return ADT[f];
+}   
+std::string Deque::back(){
+    if (n==0){
+        return "error: empty deque";
+    }
+    return ADT[r]
 }
-void back(){
-
+int Deque::size(){
+    return n;
 }
-void size(){
-    return currentN;
-}
-void empty(){
-    return currentN == 0;
+bool Deque::empty(){
+    return N == 0;
 }
 
 
