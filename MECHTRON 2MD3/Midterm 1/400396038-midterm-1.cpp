fb69707b49c3129a88aa531b618ceda1fdae6a33
Varun Kothandaraman <91991164+Nuravar@users.noreply.github.com>
2023-03-05 16:07:32 -0500

    finish formatting midterm

--- MECHTRON 2MD3/Midterm 1/400396038-midterm-1.cpp
+++ MECHTRON 2MD3/Midterm 1/400396038-midterm-1.cpp
@@ -7,34 +7,30 @@
 using namespace std;
 
 
-class Stack {
+class Stack { //stack class utlizing vector
 private:
     vector<double> data;
 
 public:
-    void push(const double value) {
+    void push(const double value) { //push function
         data.push_back(value);
     }
 
-    double pop() {
+    double pop() { //pop function with a return
         double topper = data.back();
         data.pop_back();
         return topper;
     }
 
-    double top() {
+    double top() { //top function
         return data.back();
     }
 
-    double top() const {
-        return data.back();
-    }
-
-    bool empty() const {
+    bool empty() const { //check if stack is empty
         return data.empty();
     }
 
-    size_t size() const {
+    size_t size() const { //size of vector
         return data.size();
     }
 };
@@ -55,17 +51,17 @@ class RPNEvaluator {
     bool isOneUnit(string s);
 };
 
-RPNEvaluator::RPNEvaluator(string rpn_exp){
+RPNEvaluator::RPNEvaluator(string rpn_exp){ //constructor
     expression = rpn_exp;
 }
 
-bool RPNEvaluator::isNumber(string s){
+bool RPNEvaluator::isNumber(string s){ //function to check if multi digit strings are numbers
     for( int i = 0; i < s.length(); i++ ) {
-      if(s[0]=='-'){
+      if(s[0]=='-'){ //ignores negative signs if it is the first character
         continue;
       }
-      if(s[i]!='.'){
-        if(!isdigit(s[i])) {
+      if(s[i]!='.'){ //ignores decimal places
+        if(!isdigit(s[i])) { //checks if each character in the string is a number
          return false;
         }
       } else {
@@ -75,7 +71,7 @@ bool RPNEvaluator::isNumber(string s){
    }
    return true;
 }
-bool RPNEvaluator::isOneUnit(string s){
+bool RPNEvaluator::isOneUnit(string s){ //checks if the string is one unit long
     if(s.length()==1){
         return true;
     } else {
@@ -84,39 +80,42 @@ bool RPNEvaluator::isOneUnit(string s){
 }
 
 double RPNEvaluator::Evaluate(){
-    istringstream iss(expression); //iss = expression separated by the spaces
-    while (iss){
-        string subs;
-        if (iss >> subs){ 
-            if (subs!="+"&& subs!="-" && subs!="*" && subs !="/" && isNumber(subs)){
-                cout << subs << endl;
+    istringstream iss(expression); //string string function that tokenizes/parses the input string by whitespace automatically into separate strings
+    while (iss){ //checks if there is anymore tokens to be had
+        string subs; //token container for each string
+        if (iss >> subs){//iterator through the iss stringstream, acts as a boolean to check if it is finished and assined new variable
+            if (subs!="+"&& subs!="-" && subs!="*" && subs !="/" && isNumber(subs)){ //checks if a item is a number and adds it to the stack
+                //cout << subs << endl;
                 rpn_stack.push(stod(subs)); //"-5" = -5
-            } else {
+            } else { // else if it is an operator
                 double num2,num1;
-                if(rpn_stack.size()>=2){
+                if(rpn_stack.size()>=2){ //it checks if there are two items before assigning the top two variables in the stack for operations 
                     num2 = rpn_stack.pop();
                     num1 = rpn_stack.pop();
+                } else { //if there are less than 2 items in the stack before an operation nothing can be calculatied
+                    printf("Error: malformed expression");
+                    return -1;
                 }
-                if(isOneUnit(subs)){ 
+                if(isOneUnit(subs)){ //checks if the operators are actually one unit long
                     switch (subs.at(0)){
                         case '+':
-                            rpn_stack.push(num1+num2);
+                            rpn_stack.push(num1+num2); //adds numbs
                             break;
                         case '-':
-                            rpn_stack.push(num1-num2);
+                            rpn_stack.push(num1-num2); //substracts
                             break;
                         case '*':
-                            rpn_stack.push(num1*num2);
+                            rpn_stack.push(num1*num2); //multiplies
                             break;
                         case '/':
-                            rpn_stack.push(num1/num2);
+                            rpn_stack.push(num1/num2); //divides
                             break;
-                        default:
+                        default: //else if it is a one unit character that is not an operator it is not an expression
                             printf("Error: malformed expression");
                             return -1;
                             break;
                     }
-                } else {
+                } else { //if it is some random multi digit character it returns error
                     printf("Error: malformed expression");
                     return -1;
                 }
@@ -124,7 +123,7 @@ double RPNEvaluator::Evaluate(){
         
         }
     }
-    PrintStack(rpn_stack);
+    PrintStack(rpn_stack); //prints the last number in the stack/result
     return rpn_stack.top();
 }
 
@@ -140,11 +139,12 @@ void RPNEvaluator::PrintStack(Stack s){
 }
 
 int main(){
-    string rpn_exp = "5  2 + 8 3 - * 4 /";
-    // std::cout << "Please enter an expression in RPN format: ";
-    //std::getline(std::cin, rpn_exp);
+    string rpn_exp;
+    std::cout << "Please enter an expression in RPN format: ";
+    std::getline(std::cin, rpn_exp);
     RPNEvaluator rpn(rpn_exp);
     rpn.Evaluate();
     return EXIT_SUCCESS;
 }
+
 
--- MECHTRON 2MD3/Midterm 1/400396038-midterm-3.cpp
+++ MECHTRON 2MD3/Midterm 1/400396038-midterm-3.cpp
@@ -4,7 +4,7 @@
 
 //using namespace std;
 
-class Deque {
+class Deque { //deque stack that uses a circular vector
     private:
         int n; //size
         int f; //front
@@ -12,12 +12,12 @@ class Deque {
         int N; //max size
         std::vector<std::string> ADT; //circular vector
     public:
-        Deque(int size){
+        Deque(int size){ //constructor
             N = size;
             f = 0;
             r = 0;
             n = 0;
-            ADT.reserve(size); 
+            ADT.reserve(size); //reserve is used to preassign some empty space to the vector of size N, this is to make it so that there are no out of bounds errors
         }
         void insertFront(std::string e);
         void insertBack(std::string e);
@@ -29,75 +29,55 @@ class Deque {
         bool empty();
 };
 
-void Deque::insertFront(std::string e){
-    if (n == N){
+void Deque::insertFront(std::string e){ //inserts item at the "front"
+    if (n == N){ //if full do nothing
         return;
     } 
-    f = (f - 1 + N) % N;
-    ADT[f] = e;
-    n++;
+    f = (f - 1 + N) % N; //changes front index to be "-1"
+    ADT[f] = e; //adds the item
+    n++; //increments size
 
 }
-void Deque::insertBack(std::string e){
+void Deque::insertBack(std::string e){ //inserts item at the back
     if (n == N){
         return;
     } 
-    ADT[r] = e;
-    r = (r + 1) % N;
-    n++;
+    ADT[r] = e; //since front changes before adding rear has to change after since nothing would be in that location
+    r = (r + 1) % N; //increments rear index by "+1"
+    n++; //increases size
 }
 void Deque::eraseFront(){
-    if (n==0){
+    if (n==0){ //if empty do nothing
         return;
     }
-    f = (f + 1 + N) % N;
-    n--;
+    f = (f + 1 + N) % N; //delete the front by changing the index of front to be "+1" this means that the value at the old front index will eventually be replaced
+    n--; //decreases dize
 }
 void Deque::eraseBack(){
     if (n==0){
         return;
     }
-    r = (r -1 + N) % N;
-    n--;
+    r = (r -1 + N) % N; //deletes back by changing index of rear to be "-1"
+    n--; //decreases size
 }
 std::string Deque::front(){
-    if (n==0){
+    if (n==0){ //if empty return string
         return "error: empty deque";
     }
-    return ADT[f];
+    return ADT[f]; //return front location
 }   
 std::string Deque::back(){
-    if (n==0){
+    if (n==0){ //if empty return string
         return "error: empty deque";
     }
-    return ADT[(r-1+N)%N];
+    return ADT[(r-1+N)%N]; //return rear location shifted based off cicular vector
 
 }
-int Deque::size(){
+int Deque::size(){ //returns size counter
     return n;
 }
-bool Deque::empty(){
+bool Deque::empty(){ //returns bool if size counter is zero
     return n == 0;
 }
 
 
-
-int main(){
-    Deque dq(3);
-    std::cout << "Deque is empty: " << dq.empty() << std::endl;
-    dq.insertFront("hello");
-    std::cout << "Deque size is: " << dq.size() << std::endl;
-    std::cout << "Deque front is: " << dq.front() << std::endl;
-    std::cout << "Deque back is: " << dq.back() << std::endl;
-    dq.eraseFront();
-  std::cout << "Deque size is: " << dq.size() << std::endl;
-    std::cout << "Deque is empty: " << dq.empty() << std::endl;
-    dq.insertFront("world");
-    dq.insertFront("hello");
-   dq.insertFront("help");
-    std::cout << "Deque size is: " << dq.size() << std::endl;
-    std::cout << "Deque is empty: " << dq.empty() << std::endl;
-    std::cout << "Deque front is: " << dq.front() << std::endl;
-    std::cout << "Deque back is: " << dq.back() << std::endl;
-    return 0;
-}
